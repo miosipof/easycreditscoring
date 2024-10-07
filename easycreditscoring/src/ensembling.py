c5fee4e6c9522771095c8cf5f6b1474b23f3478d
mikhail.osipov.ext@b-brave.it <mikhail.osipov.ext@b-brave.it>
2024-10-07 12:38:00 +0200

    removed unnecessary imports

--- easycreditscoring/src/ensembling.py
+++ easycreditscoring/src/ensembling.py
@@ -1,54 +1,7 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout,BatchNormalization
-from tensorflow.keras.optimizers import Adam, Nadam, AdamW
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.metrics import Recall, Precision
-from tensorflow.keras.regularizers import l2
-from tensorflow.keras.callbacks import ReduceLROnPlateau
-from tensorflow.keras.optimizers import SGD
-
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import missingno
-import matplotlib
-import matplotlib.pyplot as plt
-import plotly.express as px
 import time
-import re
 
-# Pre-Processing
-from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, MinMaxScaler
-from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, cross_validate
-from sklearn.impute import KNNImputer
-from sklearn.neighbors import LocalOutlierFactor
-from scipy import stats
-from statsmodels.stats.outliers_influence import variance_inflation_factor
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.inspection import permutation_importance
-from sklearn.utils.fixes import parse_version
-from imblearn.over_sampling import SMOTE
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import CategoricalNB
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn import tree
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.metrics import roc_auc_score
-
-from sklearn.metrics import make_scorer
-from sklearn.model_selection import cross_val_score
-
-
-# Metrics
-from sklearn.metrics import multilabel_confusion_matrix, ConfusionMatrixDisplay
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.metrics import roc_auc_score, roc_curve, precision_recall_curve, average_precision_score
-from sklearn.metrics import RocCurveDisplay
-
-from abc import ABC, abstractmethod
 
 class Ensembling:
 
