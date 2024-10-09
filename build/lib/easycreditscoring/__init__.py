e83bf7de7196bc62252ac4e5d5ee9ad147ba0586
miosipof <165400975+miosipof@users.noreply.github.com>
2024-10-09 15:34:15 +0200

    Delete build/lib/easycreditscoring directory

--- build/lib/easycreditscoring/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from .src.data_cleaning import DataCleaner
-from .src.modelzoo import ModelPipeLine
-from .src.ensembling import Ensembling
-from .src.economy import UnitEconomy
-from .src.data_cleaning import ReduceVIF
